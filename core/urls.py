d1a71c5b9cced36cb8f3c874b0e481f86c5db496
Mthunzi5 <m48209921@gmail.com>
2025-09-23 22:11:37 +0200

    Update settings.py with latest email and domain chs

--- core/urls.py
+++ core/urls.py
@@ -183,7 +183,7 @@ urlpatterns = [
     path('accounts/recover-username/', views.recover_username, name='recover_username'),
 
 
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'), name='password_reset'),
+    #path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'), name='password_reset'),
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
